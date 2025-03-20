970524465eef09e112af7411e364d36e9dc8efd3
Czyz7 <czyzhernandez@outlook.com>
2025-03-20 13:09:18 -0700

    Edited the Description

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="California Care Coordinators | Internal Resource Center"
     >
       <HomepageHeader />
       <main>
